073a4345fe502f5ede72880ae989f9c498bd6ef9
Sanjeevkumar-woks <sanjeevmanagutti@gmail.com>
2024-08-07 13:16:50 +0530

    added Task Sample ui

--- src/Components/Tasks.tsx
+++ src/Components/Tasks.tsx
@@ -1,23 +1,45 @@
 const Tasks = () => {
+  const tasks = [];
   return (
     <div className="w-screen h-screen items-center">
-      <p className="text-xl font-bold">Tasks</p>
+      <p className="text-xl font-bold ml-3 p-2">Tasks</p>
 
       <div className="flex gap-5 h-screen text-center">
         <div className=" flex-1 p-2">
-          <p className="p-4 border-b-4 rounded-md border-blue-500 text-gray-500 font-semibold">
+          <p className="p-4 border-b-4 rounded-md border-blue-500 text-lg text-gray-500 font-semibold">
             Active Task
           </p>
+          {tasks?.length ? (
+            <div></div>
+          ) : (
+            <div className=" flex justify-center items-center w-full h-full">
+              <p>No Active Tasks</p>
+            </div>
+          )}
         </div>
         <div className=" flex-1 p-2">
           <p className="p-4 border-b-4 rounded-md border-red-500 text-gray-500 font-semibold">
             Pending Task
           </p>
+          {tasks?.length ? (
+            <div></div>
+          ) : (
+            <div className=" flex justify-center items-center w-full h-full">
+              <p>No Pending Tasks</p>
+            </div>
+          )}
         </div>
         <div className=" flex-1 p-2">
           <p className="p-4 border-b-4 rounded-md border-green-500 text-gray-500 font-semibold">
             Completed Task
           </p>
+          {tasks?.length ? (
+            <div></div>
+          ) : (
+            <div className=" flex justify-center items-center w-full h-full">
+              <p>Yet to Complete</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
